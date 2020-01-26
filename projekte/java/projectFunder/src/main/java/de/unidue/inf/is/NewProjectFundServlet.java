ca92824cd724abf0dff7ea766ac4dc2da3029ac5
Nils Kruse <nils.kruse@stud.uni-due.de>
2020-01-26 21:14:10 +0100

    Some spenden

--- projekte/java/projectFunder/src/main/java/de/unidue/inf/is/NewProjectFundServlet.java
+++ projekte/java/projectFunder/src/main/java/de/unidue/inf/is/NewProjectFundServlet.java
@@ -1,9 +1,66 @@
 package de.unidue.inf.is;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.unidue.inf.is.domain.Projekt;
+import de.unidue.inf.is.stores.ProjektStore;
+import de.unidue.inf.is.stores.StoreException;
 
 public class NewProjectFundServlet extends HttpServlet
 {
     private static final long serialVersionUID = 1L;
     private String USER = HardcodedUser.get();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        List<String> report = new ArrayList<>();
+        try(ProjektStore projektStore = new ProjektStore())
+        {
+            int kennung = Integer.parseInt(request.getParameter("kennung"));
+            Projekt projekt = projektStore.getProjectForViewProject(kennung);
+            if(projekt == null)
+            {
+                report.add("Projekt nicht gefunden!");
+            }
+            else
+            {
+                if(projekt.getStatus().equalsIgnoreCase("offen"))
+                {
+                    request.setAttribute("projekt", projekt);
+                }
+                else
+                {
+                    report.add("Projekt nicht aktiv!");
+                }
+
+            }
+
+        }
+        catch(StoreException e)
+        {
+            report.add("Fehler beim Laden aus der Datenbank!");
+            e.printStackTrace();
+        }
+        catch(NumberFormatException e)
+        {
+            report.add("Ungültige Kennung!");
+        }
+        request.setAttribute("report", report);
+        request.getRequestDispatcher("/new_project_fund.ftl").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
 }
---
+++ projekte/java/projectFunder/src/main/java/de/unidue/inf/is/stores/KontoSpendenStore.java
@@ -0,0 +1,69 @@
+package de.unidue.inf.is.stores;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class KontoSpendenStore extends AbstractStore
+{
+    private final String GET_BALANCE_QUERY = "select k.guthaben from dbp064.konto k where k.inhaber=?";
+    private final String GET_FUND_QUERY = "select * from dbp064.spenden s where s.spender=? and s.projekt=?";
+    private final String INSERT_FUND = "insert into dbp064.spenden (spender,projekt,spendenbetrag,sichbtarkeit) VALUES(?,?,?,?)";
+
+    public double getBalance(String inhaber) throws StoreException
+    {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(GET_BALANCE_QUERY))
+        {
+            preparedStatement.setString(1, inhaber);
+            try(ResultSet rs = preparedStatement.executeQuery())
+            {
+                if(rs.next())
+                {
+                    return rs.getDouble(1);
+                }
+            }
+        }
+        catch(SQLException e)
+        {
+            throw new StoreException(e);
+        }
+        return -1;
+    }
+
+    public boolean fundExists(String spender, int projekt) throws StoreException
+    {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(GET_FUND_QUERY))
+        {
+            preparedStatement.setString(1, spender);
+            preparedStatement.setInt(2, projekt);
+            try(ResultSet rs = preparedStatement.executeQuery())
+            {
+                if(rs.next())
+                {
+                    return true;
+                }
+            }
+        }
+        catch(SQLException e)
+        {
+            throw new StoreException(e);
+        }
+        return false;
+    }
+
+    public void addFund(String spender, int projekt, double spendenbetrag, String sichtbarkeit) throws StoreException
+    {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(INSERT_FUND))
+        {
+            preparedStatement.setString(1, spender);
+            preparedStatement.setInt(2, projekt);
+            preparedStatement.setDouble(3, spendenbetrag);
+            preparedStatement.setString(4, sichtbarkeit);
+            preparedStatement.executeUpdate();
+        }
+        catch(SQLException e)
+        {
+            throw new StoreException(e);
+        }
+    }
+}
